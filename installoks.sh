ea370d2c5dcff505366c808014a67609d44929f0
Origin-GreenCat <79638798+Origin-GreenCat@users.noreply.github.com>
2021-02-25 21:04:47 +0800

    Add files via upload

---
+++ installoks.sh
@@ -0,0 +1,6 @@
+pkg install wget
+wget https://github.com/Origin-GreenCat/onekey-server/archive/main.zip
+unzip main.zip
+cd onekey-server-main
+unzip okserver.zip
+echo 启动服务器输入sh start.sh
---
+++ start.sh
@@ -0,0 +1,1 @@
+java -jar ct.jar -nogui
